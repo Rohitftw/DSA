bc783b154ef19378f07cb7e140c474647daf3aa3
Rohit <rohitsharmaa492@gmail.com>
2025-09-14 20:30:25 +0530

    14_sept

--- Sorting.py
+++ Sorting.py
@@ -30,29 +30,62 @@ arr = list(map(int, input("Enter elements: ").split()))
 
 
 # Merge Sort TC: O(nlogn)
-def mergesort(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    left = mergesort(arr[:mid])
-    right = mergesort(arr[mid:])
-    return merge(left, right)
+# def mergesort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     mid = len(arr) // 2
+#     left = mergesort(arr[:mid])
+#     right = mergesort(arr[mid:])
+#     return merge(left, right)
+#
+#
+# def merge(left, right):
+#     i = j = 0
+#     temp = []
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             temp.append(left[i])
+#             i += 1
+#         else:
+#             temp.append(right[j])
+#             j += 1
+#     temp.extend(left[i:])
+#     temp.extend(right[j:])
+#
+#     return temp
+#
+#
+# print(mergesort(arr))
 
-
-def merge(left, right):
-    i = j = 0
-    temp = []
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            temp.append(left[i])
-            i += 1
-        else:
-            temp.append(right[j])
-            j += 1
-    temp.extend(left[i:])
-    temp.extend(right[j:])
-
-    return temp
-
-
-print(mergesort(arr))
+# Quick Sort
+# def partition(arr, low, high):
+#     pivot = arr[low]   # pehla element ko pivot lete hain
+#     i = low
+#     j = high
+#
+#     while i < j:
+#         # left side se pivot se chhote ya equal elements ko skip karo
+#         while i <= high - 1 and arr[i] <= pivot:
+#             i += 1
+#         # right side se pivot se bade elements ko skip karo
+#         while j >= low + 1 and arr[j] > pivot:
+#             j -= 1
+#         # agar i aur j cross nahi hue toh swap
+#         if i < j:
+#             arr[i], arr[j] = arr[j], arr[i]
+#
+#     # pivot ko sahi jagah par daalna
+#     arr[low], arr[j] = arr[j], arr[low]
+#     return j
+#
+#
+# def quicksort(arr, low, high):
+#     if low < high:
+#         pIndex = partition(arr, low, high)
+#         quicksort(arr, low, pIndex - 1)   # left part
+#         quicksort(arr, pIndex + 1, high) # right part
+#
+#
+# print("Original:", arr)
+# quicksort(arr, 0, len(arr) - 1)
+# print("Sorted:", arr)
